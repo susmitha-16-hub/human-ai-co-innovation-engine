699ce20ff5e4e3006e19d3e8e90b10cc6e4756eb
Susmitha .M <susmithamothukuru@gmail.com>
2026-01-04 15:31:03 +0530

    Update RiskAnalysis.jsx

--- frontend/src/pages/RiskAnalysis.jsx
+++ frontend/src/pages/RiskAnalysis.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import axios from "axios";
+//import axios from "axios";
 
 import {
   PieChart,
@@ -243,6 +243,7 @@ export default function RiskAnalysis() {
     </div>
   );
 }
+
 
 
 
